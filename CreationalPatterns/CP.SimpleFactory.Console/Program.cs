90623a714312bb2ec317d2823561baf06d25b218
Vahid Cheshmy <vahid.cheshmy@syzygy.de>
2022-09-07 11:02:23 +0200

    Simple factory pattern.

---
+++ CreationalPatterns/CP.SimpleFactory.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CP.SimpleFactory.Lib;
+
+Console.WriteLine("".PadLeft(50,'-'));
+Console.WriteLine("Simple Factory pattern example...");
+Console.WriteLine("".PadLeft(50,'-'));
+
+ICreditCard creditCard=CreditCardFactory.CreateCreditCard("Silver");
+
+Console.WriteLine($"CreditType: {creditCard.GetCardType()}");
+Console.WriteLine($"UploadLimit: {creditCard.GetUploadLimit()} GB");
+Console.WriteLine($"AnnualCharge: {creditCard.GetAnnualCharge()} T");
+
+Console.ReadKey();
---
+++ CreationalPatterns/CP.SimpleFactory.Lib/SimpleFactory.cs
@@ -0,0 +1,58 @@
+﻿namespace CP.SimpleFactory.Lib;
+
+
+public interface ICreditCard
+{
+    string GetCardType();
+    int GetUploadLimit();
+    int GetAnnualCharge();
+}
+
+public static class CreditCardFactory
+{
+    public static ICreditCard CreateCreditCard(string creditCardType)
+    {
+        ICreditCard creditCard=null;
+        if (creditCardType == "Silver")
+        {
+            creditCard= new Silver();
+           
+        }
+        else if (creditCardType == "Gold")
+        {
+            creditCard= new Gold();
+           
+        }
+        else if (creditCardType == "Platinum")
+        {
+            creditCard= new Platinum();
+           
+        }
+        return creditCard;
+    }
+}
+
+public class Silver : ICreditCard
+{
+    public string GetCardType() => "Silver";
+
+    public int GetUploadLimit() => 15;
+
+    public int GetAnnualCharge() => 250;
+}
+public class Gold : ICreditCard
+{
+    public string GetCardType() => "Gold";
+
+    public int GetUploadLimit() => 30;
+
+    public int GetAnnualCharge() => 500;
+}
+public class Platinum : ICreditCard
+{
+    public string GetCardType() => "Platinum";
+
+    public int GetUploadLimit() => 50;
+
+    public int GetAnnualCharge() => 750;
+}
